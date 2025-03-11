a24aab19655d25b8e28dc641ce4ef92ea2e32ecc
henok-7 <nahombehailu38@gmail.com>
2025-03-11 21:15:16 +0300

    first TUE

--- lab1.cpp
+++ lab1.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main(){
-    std::cout <<"i like pizza"<<std::endl;
-    std::cout <<"i like fies"<<std::endl;
+    std::cout<<"The program analizes product sales and inventary to a store"<<std::endl;
+    std::cout<<"c++ is a compiled language"<<std::endl;
 }
